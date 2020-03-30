1b55062a508fcc3e43ab990ea79df78c334e9b84
Fabrizio Anagi <fabrizio.anagi@away.travel>
2020-03-30 18:13:30 +0200

    0.4.4

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -87,9 +87,6 @@
         const monthNames = ["Gennaio", "Febbraio", "Marzo", "Aprile", "Maggio", "Giugno",
           "Luglio", "Agosto", "Settembre", "Ottobre", "Novembre", "Dicembre"
         ];
-        console.log("TEST PER AGGIONRNAMENTO CALENDAR")
-        var t = this.date.getMonth();
-        debugger;
         return monthNames[this.date.getMonth() -1];
       },
       selectedYear() {
